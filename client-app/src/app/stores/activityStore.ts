00f21259b5436ba364b687e32e7362650635ec61
Sabintmkr <sabintmkr09@outlook.com>
2024-01-21 17:28:05 +1100

    End of section 8

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -8,7 +8,7 @@ export default class ActivityStore {
     selectedActivity: Activity | undefined = undefined;
     editMode = false;
     loading = false;
-    loadingInitial = true;
+    loadingInitial = false;
 
     constructor() {
         makeAutoObservable(this)
@@ -21,15 +21,15 @@ export default class ActivityStore {
     }
     
     loadActivities = async () => {
+        this.setLoadingInitial(true);
         try{
             const activities = await agent.Activities.list();
             runInAction(() => {
-                //this.activities = [];
+                //this.activities = []; if you are using an array instead of the Javascript Map object
         
                 activities.forEach(activity => {
-                    activity.date = activity.date.split('T')[0];
+                  this.setActivity(activity);
                    // this.activities.push(activity);
-                   this.activityRegistry.set(activity.id, activity);
                 });
 
             });
@@ -41,27 +41,42 @@ export default class ActivityStore {
         }
     };
 
+    loadActivity = async (id: string) => {
+        let activity = this.getActivity(id);
+        if(activity) {
+            this.selectedActivity = activity;
+            return activity;
+        }
+        else {
+            this.setLoadingInitial(true);
+            try{
+                activity = await agent.Activities.details(id);
+                this.setActivity(activity);
+                runInAction(() => {
+                    this.selectedActivity = activity;
+                });
+                this.setLoadingInitial(false);
+                return activity;
+            }catch (error){
+                console.log(error);
+                this.setLoadingInitial(false);
+            }
+        }
+    }
+
+    private getActivity = (id: string) => {
+        return this.activityRegistry.get(id);
+    }
+
+    private setActivity = (activity: Activity) => {
+        activity.date = activity.date.split('T')[0];
+        this.activityRegistry.set(activity.id, activity);
+    }
+
     setLoadingInitial = (state: boolean) => {
         this.loadingInitial = state;
     }
 
-    //selecting an Activity
-    selectActivity = (activityId: string) => {
-        this.selectedActivity = this.activityRegistry.get(activityId);
-    }
-
-    cancelSelectedActivity = () => {
-        this.selectedActivity = undefined;
-    }
-
-    openForm = (id?: string) => {
-        id ? this.selectActivity(id) : this.cancelSelectedActivity();
-        this.editMode = true;
-    }
-
-    closeForm = () => {
-        this.editMode = false;
-    }
 
     createActivity = async (activity: Activity) => {
         this.loading = true;
@@ -107,7 +122,6 @@ export default class ActivityStore {
             await agent.Activities.delete(id);
             runInAction(() => {
                 this.activityRegistry.delete(id);
-                if(this.selectedActivity?.id === id) this.cancelSelectedActivity;
                 this.loading = false;
             })
 
--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -9,13 +9,22 @@ import {
 } from "semantic-ui-react";
 import { useStore } from "../../../app/stores/store";
 import LoadingComponent from "../../../app/layout/LoadingComponent";
+import { observer } from "mobx-react-lite";
+import { Link, useParams } from "react-router-dom";
+import { useEffect } from "react";
 
 
-export default function ActivitDetails() {
+export default observer (function ActivitDetails() {
   const {activityStore} = useStore();
-  const {selectedActivity: activity, openForm, cancelSelectedActivity} = activityStore;
+  const {selectedActivity: activity, loadActivity, loadingInitial} = activityStore;
+  const {id} = useParams();
 
-  if(!activity) return <LoadingComponent />
+  useEffect(() => {
+    if (id) loadActivity(id);
+  }, [id, loadActivity])
+
+  if(loadingInitial || !activity) return <LoadingComponent />;
+
   return (
     <Card fluid>
       <Image src={`/assets/categoryImages/${activity.category}.jpg`} />
@@ -28,10 +37,10 @@ export default function ActivitDetails() {
       </CardContent>
       <CardContent extra>
         <Button.Group widths='2'>
-            <Button onClick={() => openForm(activity.id)} basic color='blue' content='Edit' />
-            <Button onClick={cancelSelectedActivity}  basic color='grey' content='Cancel' />
+            <Button as={Link} to={`/manage/${activity.id}`} basic color='blue' content='Edit' />
+            <Button as={Link} to='/activities' basic color='grey' content='Cancel' />
         </Button.Group>
       </CardContent>
     </Card>
   )
-}
+})
